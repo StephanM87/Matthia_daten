23b2d973bdaba7f498a886a97301028118ffdbef
MRieb <m.rieb@fz-juelich.de>
2022-03-04 10:40:12 +0100

    Unnötige Lines gel

--- skripte.py
+++ skripte.py
@@ -12,75 +12,20 @@ import re
 class FileSorter:
 
     '''
-        This class handles thawdawdwdadwawdawdawdawdawadwdawdawdawdawdawdawdawdawdawdae sorting of the Image Files. 
+        This class handles sorting of the Image Files. 
         The target of this package is to create an ???? image of the measurements, therefore the files need to be sorted corretly. 
         This is a challenge since these files have to be sorted according to the substrate concentrations in their path.
 
         args:
             folder: String; String which directs to the folder in which the measurement files are located
-Stephan iost der Beste
         returns:
             None
     '''
 
-    def __init__(self, folder):
-        self.folder = folder
-
-    
-    def upload_files(self):
-        '''
-            uploads the measurement files from the local storage and returns an unsorted list with the measurement files
-
-            args:
-                folder: String; String which directs to the folder in which the measurement files are located
-            
-            returns:
-                files: List; List containing the unsorted files
-
-        '''
-        folder = pathlib.Path(repr(self.folder))
-        files = [file for file in folder.glob('*.nd2')] #hier werden die Datei namen der einzelen Dateien hinterlegt
-        return files
-
-    def sorting_strings(self):
-
-        '''
-            Defines the regular expression list which should be screened for
-
-            args:
-                None
-            
-            returns:
-                reg_expression: List; returns list with search patterns
-
-        '''
-        reg_expression = ["_0,1mM", "_0,5mM", "_0,75mM" "_0,25mM", "_0,05mM", "_0,075mM", "_1mM", "_2,5mM", "_5mM", "_7,5mM", "_10mM", "_25mM", "_50mM", "_75mM", "_100mM", "_250mM"]
-
-        return reg_expression
-
-    
-    
-
-
-
-
-
 folder = pathlib.Path(r"F:\Juelich\soluble MP38.8.10") #in diesem Ordner liegen die Dateien, muss angepasst werden!
 files = [file for file in folder.glob('*.nd2')] #hier werden die Datei namen der einzelen Dateien hinterlegt
 
-
-fig, ax = plt.subplots(1,len(files), figsize = (20,4), dpi=150)
-
-ergebnisse  = {}
-ergebnisse2 = {}
-images      = {}
-
-
-list_concentration = ["^0,001$", "^0,0025$", "0,005", "0,0075", "0,01", "0,025", "0,05", "0,075", "0,1", "0,25", "0,5", "0,75", "1", "2,5", "5", "7,5", "10", "25", "50", "75", "100", "250", "500", "750", "1000"]
-
-newList = ["_0,1mM", "_0,5mM", "_0,75mM" "_0,25mM", "_0,05mM", "_0,075mM", "_1mM", "_2,5mM", "_5mM", "_7,5mM", "_10mM", "_25mM", "_50mM", "_75mM", "_100mM", "_250mM"]
-
-
+newList = ["_0,05mM", "_0,075mM","_0,1mM", "_0,5mM", "_0,75mM", "_0,25mM", "_1mM", "_2,5mM", "_5mM", "_7,5mM", "_10mM", "_25mM", "_50mM", "_75mM", "_100mM", "_250mM", '_500mM', '750mM', '_1000mM']
 correct_list = []
 
 for i in newList:
@@ -99,56 +44,6 @@ for i in newList:
             None
 
 print(correct_list, "die richtige liste")
-
-        
-
-
-
-unsorted_files = []
-for i in files:
-    # c_date is the creation date of the respective file
-    c_date = time.ctime(os.path.getctime(i))
-    # path is the element of the list
-    path = i
-    string = str(i)
-    # Regular expressions
-    string = str(i)
-    ret = re.search("0,05", string)
-    #print(ret)
-
-    new_entry = {}
-    new_entry["date"] = c_date
-    new_entry["path"] = path
-    unsorted_files.append(new_entry)
-
-#sorted_files = unsorted_files.sort(key=date, reverse=False)
-
-sorted_files= sorted(unsorted_files, key=lambda d: d['date'])
-
-
-
-
-
-
-
-for i in files:
-    string = str(i)
-    ret = re.search("0,05", string)
-    ret01 = re.search("0,1", string)
-    if ret != None:
-        None
-        #print(ret, "0,05 konzentration")
-
-    elif ret01!=None:
-        None
-        #print(ret01, "0,1 konzentration")
-    
-
-    
-
-
-
-
 
 '''
 for i in range(len(files)):
